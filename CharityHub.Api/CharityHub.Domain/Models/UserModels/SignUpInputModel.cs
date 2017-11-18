4140a8c22afba1f497b8752f0eb833e790946db6
Damian Dmochowski <ddmochowski@pretius.com>
2017-11-18 16:33:25 +0100

    [API] changed lastname on surname

--- CharityHub.Api/CharityHub.Domain/Models/UserModels/SignUpInputModel.cs
+++ CharityHub.Api/CharityHub.Domain/Models/UserModels/SignUpInputModel.cs
@@ -11,7 +11,7 @@ namespace CharityHub.Domain.Models.UserModels
         public string EmailAddress { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
-        public string LastName { get; set; }
+        public string Surname { get; set; }
         //public DateTime? BirthDate { get; set; }
         /// <summary>
         /// Male, Female
