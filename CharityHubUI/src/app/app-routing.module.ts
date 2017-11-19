2857a24f77e335ee0c1856dc6dfd01c5f5aa47e2
Krystian Rytel <krystian.rytel@gmail.com>
2017-11-19 02:54:38 +0100

    [UI] Hotfix

--- CharityHubUI/src/app/app-routing.module.ts
+++ CharityHubUI/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'charity/news', pathMatch: 'full' },
+  { path: '', redirectTo: 'charity/organizations', pathMatch: 'full' },
   { path: 'user', loadChildren: 'app/home/home.module#HomeModule' },
   { path: 'charity', loadChildren: 'app/charity/charity.module#CharityModule' }
 ];
